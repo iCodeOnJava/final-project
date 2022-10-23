dc0e5b4e8740e62d3bd38935451fb1aa36755bf0
TK <tk@gmail.com>
2022-10-23 12:35:20 +0300

    [add] - income data

---
+++ src/main/java/com/example/endofthegame/entity/enumeration/IncomeCategory.java
@@ -0,0 +1,12 @@
+package com.example.endofthegame.entity.enumeration;
+
+//TODO:
+public enum IncomeCategory {
+    SALARY,
+    GIFT,
+    GOVERNMENT_SUPPORT,
+    ADHOC_WORK,
+    RENT,
+    DIVIDENDS,
+    OTHER
+}
